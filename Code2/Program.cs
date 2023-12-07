294f5f7040d998113b42997e759d30d2dc67f426
Aliaksandr Salata <sasvanis@gmail.com>
2023-12-07 23:04:38 +0300

    Code_02

---
+++ Code2/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Инициализация исходного массива строк
+        string[] origArray = {"retro", "hello", "wq", "por", "hiller"};
+        //Создание итогового массива строк
+        string[] newArray = new string[origArray.Length];
+        int newIndex = 0;
+
+        //Итерирование по исходному массиву
+        for (int i = 0; i < origArray.Length; i++)
+
+        {
+            // Проверка длины строки
+            if (origArray[i].Length <= 3)
+
+            {
+                //Добавление строки в новый массив
+                newArray[newIndex] = origArray[i];
+                newIndex++;
+            }
+        }
+        //Вывод результата
+        Console.WriteLine("Новый массив строк:");
+        for (int i = 0; i < newIndex; i++)
+    {
+        Console.WriteLine(newArray[i]);
+    }
+    }
+}
